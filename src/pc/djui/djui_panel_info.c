58a8dc880496a386d59025e5eb20dd8dadb34aeb
Toad114514 <xiaolan2332021@163.com>
2026-08-05 21:00:35 +0800

    wq

--- src/pc/djui/djui_panel_info.c
+++ src/pc/djui/djui_panel_info.c
@@ -11,7 +11,7 @@ void djui_panel_info_create(struct DjuiBase *caller) {
     {
         //snprintf(sInfo, 512, "\
 //感谢游玩蘑菇云译社的汉化版本！更新&聊天加QQ群聊981281124\n(资源只允许在官方群聊下载，如果你是通过别的渠道下载的，请加入群聊向我们申诉)\n特别鸣谢：梅塔的长名字、小花jacob、SGF3、xXram2dieXx、狗哥又玩又爱玩、小扬awa、超级屑的屑蓝猫、一只普通的刺猬、Toad114514、216、54wxw、Torbi、大聪明H、xiao-Link、雪狼SnowWolf、脚滑的冰块、rew_lezi64以及所有Mod汉化者\n当前汉化版本:1.0.1");
-        snprintf(sInfo, 512, "Welcome to using Derect Client!!\n此版本仅作为本作者用于学习 C/C++ 知识、Dear ImGUI使用，并不是挂端！请在下载之后24小时内删除此程序！\nMade By toadXtech64(Github: toad114514)\n基于上善若/梅塔的长名字的汉化版本");
+        snprintf(sInfo, 512, "Welcome to using Derect Client!!\n此客户端仅作为个人用于学习用途，请不要使用此客户端来进行恶意非法盈利！请在下载之后24小时内删除此程序！\n此客户端完全遵守 Minecraft Eula 协议。\nMade By toadXtech64(Github: toad114514)\n此构建基于上善若/梅塔的长名字的汉化版本");
         struct DjuiText* text = djui_text_create(body, sInfo);
         djui_base_set_location(&text->base, 0, 0);
         djui_base_set_size(&text->base, (DJUI_DEFAULT_PANEL_WIDTH * (configDjuiThemeCenter ? DJUI_THEME_CENTERED_WIDTH : 1)) - 64, 400);
