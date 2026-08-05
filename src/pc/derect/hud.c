f8f744e2f9fb06071f69b0f0904088188e3d65d0
Toad114514 <xiaolan2332021@163.com>
2026-08-06 03:49:04 +0800

    idkfuck

--- src/pc/derect/hud.c
+++ src/pc/derect/hud.c
@@ -7,14 +7,12 @@
 #include "module.h"
 
 void Derect_RenderHUD(void) {
-    ImGuiIO* io = igGetIO();
-
-    // 1. 锚点定位在右上角，距离边缘 15px
-    igSetNextWindowPos((ImVec2){io->DisplaySize.x - 15.0f, 15.0f}, ImGuiCond_Always, (ImVec2){1.0f, 0.0f});
-    
     int count = 0;
+    // moduless
     Module* modules = Module_GetAll(&count);
     ImGuiIO* io = igGetIO();
+    
+    igSetNextWindowPos((ImVec2){io->DisplaySize.x - 15.0f, 15.0f}, ImGuiCond_Always, (ImVec2){1.0f, 0.0f});
     
     // 2. HUD 必备窗口属性：无装饰、自动尺寸、无背景、不响应鼠标输入（透传到游戏）、不可移动
     ImGuiWindowFlags hud_flags = ImGuiWindowFlags_NoDecoration 
