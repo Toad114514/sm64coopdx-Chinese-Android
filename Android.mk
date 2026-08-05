a5e38dc35beff3f56ab1e45a20f77eae14bad1e2
Toad114514 <xiaolan2332021@163.com>
2026-08-05 13:20:01 +0800

    android.mk update

--- Android.mk
+++ Android.mk
@@ -43,16 +43,11 @@ LOCAL_MODULE := curl
 LOCAL_SRC_FILES := $(LOCAL_PATH)/lib/curl/android/$(TARGET_ARCH_ABI)/libcurl.so
 include $(PREBUILT_SHARED_LIBRARY)
 
+# --- MAIN MODULE ---
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
-### Build Options ###
 
-# These options can either be changed by modifying the makefile, or
-# by building with 'make SETTING=value'. 'make clean' may be required.
-
-# Version of the game to build
 VERSION ?= us
-
 TOUCH_CONTROLS ?= 1
 
 ifeq ($(VERSION),jp)
@@ -70,16 +65,23 @@ endif
 
 PC_BUILD_DIR := $(LOCAL_PATH)/build/$(VERSION)_pc
 
-SRC_DIRS := src src/engine src/game src/audio src/menu src/buffers actors levels bin bin/$(VERSION) data assets sound src/pc src/pc/gfx src/pc/audio src/pc/controller src/pc/fs src/pc/mods src/pc/dev src/pc/network src/pc/network/packets src/pc/network/socket src/pc/network/coopnet src/pc/utils src/pc/utils/miniz src/pc/djui src/pc/lua src/pc/lua/utils src/pc/mumble include/android_execinfo src/pc/cimgui src/pc/cimgui/imgui src/pc/cimgui/imgui/backends src/pc/derect
-SRC_DIRS := $(addprefix $(LOCAL_PATH)/,$(SRC_DIRS))
+# 1. 源码目录声明
+SRC_DIRS := src src/engine src/game src/audio src/menu src/buffers actors levels bin bin/$(VERSION) data assets sound \
+            src/pc src/pc/gfx src/pc/audio src/pc/controller src/pc/fs src/pc/mods src/pc/dev \
+            src/pc/network src/pc/network/packets src/pc/network/socket src/pc/network/coopnet \
+            src/pc/utils src/pc/utils/miniz src/pc/djui src/pc/lua src/pc/lua/utils src/pc/mumble \
+            include/android_execinfo src/pc/ui \
+            src/pc/cimgui src/pc/cimgui/imgui src/pc/cimgui/imgui/backends src/pc/derect
+
+SRC_DIRS_ABS := $(addprefix $(LOCAL_PATH)/,$(SRC_DIRS))
 
 GODDARD_SRC_DIRS := src/goddard src/goddard/dynlists
-GODDARD_SRC_DIRS := $(addprefix $(LOCAL_PATH)/,$(GODDARD_SRC_DIRS))
+GODDARD_SRC_DIRS_ABS := $(addprefix $(LOCAL_PATH)/,$(GODDARD_SRC_DIRS))
 
-# Source code files
+# 2. 搜寻 C 和 C++ 源文件
 LEVEL_C_FILES := $(wildcard $(LOCAL_PATH)/levels/*/leveldata.c) $(wildcard $(LOCAL_PATH)/levels/*/script.c) $(wildcard $(LOCAL_PATH)/levels/*/geo.c)
-C_FILES := $(foreach dir,$(SRC_DIRS),$(wildcard $(dir)/*.c)) $(LEVEL_C_FILES)
-CXX_FILES := $(foreach dir,$(SRC_DIRS),$(wildcard $(dir)/*.cpp))
+C_FILES       := $(foreach dir,$(SRC_DIRS_ABS),$(wildcard $(dir)/*.c)) $(LEVEL_C_FILES)
+CXX_FILES     := $(foreach dir,$(SRC_DIRS_ABS),$(wildcard $(dir)/*.cpp))
 
 ULTRA_C_FILES := \
   alBnkfNew.c \
@@ -94,22 +96,48 @@ ULTRA_C_FILES := \
 
 C_FILES := $(filter-out $(LOCAL_PATH)/src/game/main.c,$(C_FILES))
 ULTRA_C_FILES := $(addprefix $(LOCAL_PATH)/lib/src/,$(ULTRA_C_FILES))
-
-GODDARD_C_FILES := $(foreach dir,$(GODDARD_SRC_DIRS),$(wildcard $(dir)/*.c))
+GODDARD_C_FILES := $(foreach dir,$(GODDARD_SRC_DIRS_ABS),$(wildcard $(dir)/*.c))
 
 GENERATED_C_FILES := $(PC_BUILD_DIR)/assets/mario_anim_data.c $(PC_BUILD_DIR)/assets/demo_data.c \
   $(addprefix $(LOCAL_PATH)/bin/,$(addsuffix _skybox.c,$(notdir $(basename $(wildcard $(LOCAL_PATH)/textures/skyboxes/*.png)))))
 
+# 3. 将绝对路径统一转换为 NDK 需要的相对路径
+ALL_SRC_FILES := $(C_FILES) $(CXX_FILES) $(GENERATED_C_FILES) $(ULTRA_C_FILES) $(GODDARD_C_FILES)
+LOCAL_SRC_FILES := $(ALL_SRC_FILES:$(LOCAL_PATH)/%=%)
+
+# 4. 依赖库与编译参数设置
 LOCAL_SHORT_COMMANDS := true
 LOCAL_SHARED_LIBRARIES := sdl2 curl
 LOCAL_STATIC_LIBRARIES := lua5.3.5 coopnet libjuice
 LOCAL_LDLIBS := -lEGL -lGLESv3 -llog -lz -pthread -rdynamic -ldl -landroid
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/include/android_execinfo $(LOCAL_PATH)/src $(LOCAL_PATH)/sound $(LOCAL_PATH)/lib/lua/include $(LOCAL_PATH)/lib/coopnet/include $(LOCAL_PATH)/lib/sdl2/include $(LOCAL_PATH)/lib/curl/include $(PC_BUILD_DIR) $(PC_BUILD_DIR)/include $(JNI_H_INCLUDE) src/pc/cimgui/
-LOCAL_CFLAGS := -DNON_MATCHING -DAVOID_UB -DTARGET_LINUX -DTARGET_ANDROID -DENABLE_OPENGL -DWIDESCREEN -DF3DEX_GBI_2E -D_LANGUAGE_C -DNO_SEGMENTED_MEMORY -D$(VERSION_DEF) -DSTDC_HEADERS -DDYNOS -DCOOP -DCOOPNET -DUSE_GLES -DTEXTURE_FIX -DBETTERCAMERA -DEXT_OPTIONS_MENU -DIMMEDIATELOAD -DRAPI_GL=1 -DWAPI_SDL2=1 -DAAPI_SDL2=1 -DCAPI_SDL2 -DHAVE_SDL2=1 -DIMGUI_IMPL_OPENGL_ES3 -O3 -w
-LOCAL_CPPFLAGS += -std=c++17 -DIMGUI_IMPL_OPENGL_ES3
+
+# 补全 ImGui 和 cimgui 的头文件包含路径
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
+                    $(LOCAL_PATH)/include/android_execinfo \
+                    $(LOCAL_PATH)/src \
+                    $(LOCAL_PATH)/sound \
+                    $(LOCAL_PATH)/lib/lua/include \
+                    $(LOCAL_PATH)/lib/coopnet/include \
+                    $(LOCAL_PATH)/lib/sdl2/include \
+                    $(LOCAL_PATH)/lib/curl/include \
+                    $(PC_BUILD_DIR) \
+                    $(PC_BUILD_DIR)/include \
+                    $(JNI_H_INCLUDE) \
+                    $(LOCAL_PATH)/src/pc/cimgui \
+                    $(LOCAL_PATH)/src/pc/cimgui/imgui \
+                    $(LOCAL_PATH)/src/pc/cimgui/imgui/backends \
+                    $(LOCAL_PATH)/src/pc/ui
+
+LOCAL_CFLAGS := -DNON_MATCHING -DAVOID_UB -DTARGET_LINUX -DTARGET_ANDROID -DENABLE_OPENGL -DWIDESCREEN \
+                -DF3DEX_GBI_2E -D_LANGUAGE_C -DNO_SEGMENTED_MEMORY -D$(VERSION_DEF) -DSTDC_HEADERS \
+                -DDYNOS -DCOOP -DCOOPNET -DUSE_GLES -DTEXTURE_FIX -DBETTERCAMERA -DEXT_OPTIONS_MENU \
+                -DIMMEDIATELOAD -DRAPI_GL=1 -DWAPI_SDL2=1 -DAAPI_SDL2=1 -DCAPI_SDL2 -DHAVE_SDL2=1 \
+                -DIMGUI_IMPL_OPENGL_ES3 -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS -O3 -w
+
+LOCAL_CPPFLAGS += -std=c++17 -DIMGUI_IMPL_OPENGL_ES3 -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS
 
 ifeq ($(TOUCH_CONTROLS),1)
   LOCAL_CFLAGS += -DTOUCH_CONTROLS
 endif
-LOCAL_SRC_FILES := $(C_FILES) $(CXX_FILES) $(GENERATED_C_FILES) $(ULTRA_C_FILES) $(GODDARD_C_FILES)
+
 include $(BUILD_SHARED_LIBRARY)
