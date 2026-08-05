0dfe168a661b2874849691492305d19689ec2188
Toad114514 <xiaolan2332021@163.com>
2026-08-06 06:18:22 +0800

    fontscale1.8f

--- src/pc/derect/ui.c
+++ src/pc/derect/ui.c
@@ -40,17 +40,13 @@ static bool mod_guiblur = true;
 
 void derect_panel_render(bool* p_open) {
     if (!*p_open) return;
-    
-    ImFont* font = igGetFont();
-    if (font) {
-        font->Scale = 1.8f; // 设置字体缩放比例
-    }
 
     ImGuiIO* io = igGetIO();
 
     // ================= 1. 顶部导航栏 =================
     igSetNextWindowPos((ImVec2){io->DisplaySize.x * 0.5f, 20}, ImGuiCond_Always, (ImVec2){0.5f, 0.0f});
     igBegin("TopBar", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings);
+    igSetWindowFontScale(1.8f);
     
     int tab_count = sizeof(top_tabs) / sizeof(top_tabs[0]);
     
--- src/pc/derect/widget.c
+++ src/pc/derect/widget.c
@@ -72,6 +72,8 @@ void VapeUI_RenderCategoryPanel(ModuleCategory target_cat, ImVec2 pos) {
     // 自定义分类 Header
     if (visible) {
         // 自定义 Header
+        igSetWindowFontScale(1.8f);
+        
         igTextDisabled("^");
         igSameLine(0, 6);
         igText("%s", cat_name);
